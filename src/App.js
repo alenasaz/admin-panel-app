a10f5ef81bb234c11bab363df0fe758dacaad7ab
alenasaz <aLeNka_saZaNova@mail.ru>
2021-11-11 11:03:58 +0300

    Deleted some files

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import Table from './components/table';
 import Footer from './components/footer';
 import HeaderFilters from './components/header-filters';
 import Filters from './components/filters';
-import FooterPagination from './components/footer-pagination';
 
 function App() {
   return (
     <div className="App">
-    <Header></Header>
-    <HeaderFilters></HeaderFilters>
-    <Filters></Filters>
-    <Table></Table>
-    <Footer></Footer>
+    <Header/>
+    <HeaderFilters/>
+    <Filters/>
+    <Table/>
+    <Footer/>
     </div>
   );
 }
