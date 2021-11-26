ecd54bc61f5c275f097d53f3cc372f9123d44758
alenasaz <aLeNka_saZaNova@mail.ru>
2021-11-26 17:17:30 +0300

    Added input component

--- src/App.js
+++ src/App.js
@@ -9,17 +9,15 @@ import { Form } from './components/Form/Form';
 function App() {
   return (
     <div className="App">
-
-    <Form/>
+    <Header/>
+    <HeaderFilters/>
+    <Filters/>
+    <Table/>
+    <Footer/>
     </div>
   );
 }
 
 export default App;
 
-
-    // <Header/>
-    // <HeaderFilters/>
-    // <Filters/>
-    // <Table/>
-    // <Footer/>
+//<Form/>
---
+++ src/components/Input/InputIconData.js
@@ -0,0 +1,38 @@
+export const InputIconClass = {
+  filter: {
+    classNameIcon: "filter__input-icon_incorrect",
+  },
+  noicon: {
+    classNameIcon: "filter__input-icon_incorrect_no",
+  },
+  dropdown: {
+    classNameIcon: "filter__input-icon_incorrect",
+  },
+  header: {
+    classNameIcon: "filter__input-icon_incorrect",
+  },
+};
+
+export const InputIconData = {
+  filter: {
+    circle: "",
+    path: <path d="M12.5 3.5L3.5 12.5M3.5 3.5L12.5 12.5" fill="none" />,
+  },
+  noicon: {
+    circle: "",
+    path: <path d="M12.5 3.5L3.5 12.5M3.5 3.5L12.5 12.5" fill="none" />,
+  },
+  dropdown: {
+    circle: "",
+    path: <path d="M5 6H11V6.5L8.25 11H7.75L5 6.5V6Z" stroke="none" />,
+  },
+  header: {
+    circle: "",
+    path: (
+      <path
+        d="M10.0355 10.0355C11.9882 8.08291 11.9882 4.91709 10.0355 2.96447C8.08291 1.01184 4.91709 1.01184 2.96447 2.96447C1.01184 4.91709 1.01184 8.08291 2.96447 10.0355C4.91709 11.9882 8.08291 11.9882 10.0355 10.0355ZM10.0355 10.0355L14 14M14 14L14.5 13.5L11.5 10.5M14 14L13.5 14.5L10.5 11.5"
+        fill="none"
+      />
+    ),
+  },
+};
